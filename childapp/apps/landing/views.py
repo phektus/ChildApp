118f443b6eb72229333d9755d72a8d5da47ab920
Arbie Samong <phektus@gmail.com>
2012-07-07 00:31:52 +0800

    added confirmation on delete; added link to edit

--- childapp/apps/landing/views.py
+++ childapp/apps/landing/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 def index(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/children/')
     return render_to_response('index.html', context_instance=RequestContext(request))
 
