de8b0068413d936850781373dd6a53ab8d2cdc5b
Arbie Samong <phektus@gmail.com>
2012-07-07 00:44:45 +0800

    added heroku settings

--- childapp/conf/settings.py
+++ childapp/conf/settings.py
@@ -86,3 +86,7 @@ INSTALLED_APPS = (
     'registration',
     'activelink',
 )
+
+# for heroku
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
