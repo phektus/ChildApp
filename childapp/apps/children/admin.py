630374654c50998b8461a2e80ae53e208d71045b
Arbie Samong <phektus@gmail.com>
2012-07-07 10:45:08 +0800

    added less files; added date widget for birthday

---
+++ childapp/apps/children/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from childapp.apps.children.models import Child
+
+admin.site.register(Child)
--- childapp/apps/children/forms.py
+++ childapp/apps/children/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.contrib.admin import widgets
 
 from childapp.apps.children.models import Child
 
 class ChildForm(forms.ModelForm):
-
+    birthday = forms.DateField(widget=widgets.AdminDateWidget())
     class Meta:
         model = Child
         exclude = ['parent']
