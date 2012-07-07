cdef923d2191d25bd73cc249fe913e0ece1724b3
Arbie Samong <phektus@gmail.com>
2012-07-07 10:32:13 +0800

    fixed redirect on login

--- childapp/apps/children/views.py
+++ childapp/apps/children/views.py
@@ -2,17 +2,20 @@ from django.core.urlresolvers import resolve, reverse
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from childapp.apps.children.models import Child
 from childapp.apps.children.forms import ChildForm
 
+@login_required
 def index(request):
-    children = Child.objects.all()
+    children = Child.objects.all().filter(parent=request.user)
     c = RequestContext(request, {
         "children": children,
     })
     return render_to_response('children/index.html', context_instance=c)
 
+@login_required
 def view(request, id=None):
     child = Child.objects.get(id=int(id))
     c = RequestContext(request, {
@@ -20,6 +23,7 @@ def view(request, id=None):
     })
     return render_to_response('children/view.html', context_instance=c)
 
+@login_required
 def edit(request, id=None):
     if id is not None:
         child = Child.objects.get(id=id)
@@ -43,6 +47,7 @@ def edit(request, id=None):
 
     return render_to_response('children/edit.html', context_instance=c)
 
+@login_required
 def delete(request, id=None):
     if id:
         child = Child.objects.get(id=id)
